5fd164b242b118c334c0f80b4fba791b8a99db2a
inghamn <inghamn@bloomington.in.gov>
2012-03-23 16:48:08 -0400

    Added working Preferences

--- src/gov/in/bloomington/open311/MainActivity.java
+++ src/gov/in/bloomington/open311/MainActivity.java
@@ -1,34 +1,33 @@
+/**
+ * @copyright 2012 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/gpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ */
 package gov.in.bloomington.open311;
 
 import android.app.Activity;
 import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 
 public class MainActivity extends Activity {
-	SharedPreferences preferences;
-	
+
     /** Called when the activity is first created. */
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        
-        preferences = PreferenceManager.getDefaultSharedPreferences(this);
     }
-    
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
     	MenuInflater inflater = getMenuInflater();
     	inflater.inflate(R.menu.mainmenu, menu);
     	return true;
     }
-    
+
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
     	switch (item.getItemId()) {
--- src/gov/in/bloomington/open311/MyPreferencesActivity.java
+++ src/gov/in/bloomington/open311/MyPreferencesActivity.java
@@ -1,12 +1,45 @@
+/**
+ * @copyright 2012 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/gpl.txt GNU/AGPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ */
 package gov.in.bloomington.open311;
+
+import java.util.Map;
 
 import android.os.Bundle;
 import android.preference.PreferenceActivity;
+import android.preference.PreferenceManager;
+import android.content.SharedPreferences;
+import android.content.SharedPreferences.OnSharedPreferenceChangeListener;
 
-public class MyPreferencesActivity extends PreferenceActivity {
+public class MyPreferencesActivity extends PreferenceActivity implements OnSharedPreferenceChangeListener {
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		addPreferencesFromResource(R.xml.preferences);
+		SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+		
+		Map<String, ?> map = preferences.getAll();
+		for (Map.Entry<String, ?> entry:map.entrySet()) {
+			String key = entry.getKey();
+			getPreferenceScreen().findPreference(key).setSummary(preferences.getString(key, ""));
+		}
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+		getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+		getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
+	}
+	
+	public void onSharedPreferenceChanged(SharedPreferences p, String key) {
+		getPreferenceScreen().findPreference(key).setSummary(p.getString(key, ""));
 	}
 }
