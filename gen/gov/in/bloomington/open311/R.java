18e4a10a8d908d16d9d288920985a558ae179b8e
inghamn <inghamn@bloomington.in.gov>
2012-03-27 13:02:14 -0400

    Removed MyServers from menu

--- gen/gov/in/bloomington/open311/R.java
+++ gen/gov/in/bloomington/open311/R.java
@@ -14,7 +14,6 @@ public final class R {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int myServers=0x7f070001;
         public static final int preferences=0x7f070000;
     }
     public static final class layout {
