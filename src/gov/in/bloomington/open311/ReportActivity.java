4e7fea816e3cfdae727854bf1bf4c9dd81be2641
inghamn <inghamn@bloomington.in.gov>
2012-03-28 15:27:13 -0400

    Added tabs to the interface

---
+++ src/gov/in/bloomington/open311/ReportActivity.java
@@ -0,0 +1,12 @@
+/**
+ * @copyright 2012 City of Bloomington, Indiana
+ * @license http://www.gnu.org/licenses/gpl.txt GNU/GPL, see LICENSE.txt
+ * @author Cliff Ingham <inghamn@bloomington.in.gov>
+ */
+package gov.in.bloomington.open311;
+
+import android.app.TabActivity;
+
+public class ReportActivity extends TabActivity {
+
+}
