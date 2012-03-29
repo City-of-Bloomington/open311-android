115af1a7386ec71ce410b78cdb7da111711d70c5
inghamn <inghamn@bloomington.in.gov>
2012-03-29 11:25:11 -0400

    Applied the new app icon

--- gen/gov/in/bloomington/open311/R.java
+++ gen/gov/in/bloomington/open311/R.java
@@ -11,8 +11,8 @@ public final class R {
     public static final class attr {
     }
     public static final class drawable {
-        public static final int ic_dialog_map=0x7f020000;
-        public static final int ic_launcher=0x7f020001;
+        public static final int georeporter_icon=0x7f020000;
+        public static final int ic_dialog_map=0x7f020001;
         public static final int ic_menu_camera=0x7f020002;
         public static final int ic_menu_home=0x7f020003;
         public static final int ic_menu_notifications=0x7f020004;
