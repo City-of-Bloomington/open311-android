befd1af43fc6914d3e73a1efca9abe05e89dca03
Fransiska P.W. Hadiwidjana <fransiskapw@gmail.com>
2012-07-08 19:38:59 -0700

    resolve warning at my reports activity

--- src/gov/in/bloomington/open311/view/MyReports.java
+++ src/gov/in/bloomington/open311/view/MyReports.java
@@ -19,7 +19,6 @@ import android.app.AlertDialog;
 import android.content.Intent;
 import android.content.DialogInterface;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
