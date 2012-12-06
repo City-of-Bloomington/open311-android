f3d500a0526146092e0658f596b73d10511efcef
inghamn <inghamn@bloomington.in.gov>
2012-12-06 16:33:07 -0500

    Implemented changing splash image for each server in available_servers.

--- open311-android/src/gov/in/bloomington/georeporter/activities/MainActivity.java
+++ open311-android/src/gov/in/bloomington/georeporter/activities/MainActivity.java
@@ -16,10 +16,14 @@ import android.app.ProgressDialog;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends BaseActivity {
-	@Override
+    private static String SPLASH_IMAGE = "splash_image";
+
+    @Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
@@ -36,6 +40,13 @@ public class MainActivity extends BaseActivity {
 		}
 		else {
 			new EndpointLoader().execute(current_server);
+			
+            String imageName = current_server.optString(SPLASH_IMAGE);
+            Log.i("MainActivity", imageName);
+            if (imageName != "") {
+                ImageView splash = (ImageView) findViewById(R.id.splash);
+                splash.setImageResource(getResources().getIdentifier(imageName, "drawable", getPackageName()));
+            }
 		}
 	}
 	
@@ -51,7 +62,7 @@ public class MainActivity extends BaseActivity {
 	}
 	
 	private class EndpointLoader extends AsyncTask<JSONObject, Void, Boolean> {
-		ProgressDialog dialog;
+		private ProgressDialog dialog;
 		
 		@Override
 		protected void onPreExecute() {
