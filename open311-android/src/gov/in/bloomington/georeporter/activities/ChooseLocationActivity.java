c08308bd3085ce6ee3ca1c404ea898e6123aeb01
inghamn <inghamn@bloomington.in.gov>
2012-12-11 11:31:52 -0500

    Improved user location detection

--- open311-android/src/gov/in/bloomington/georeporter/activities/ChooseLocationActivity.java
+++ open311-android/src/gov/in/bloomington/georeporter/activities/ChooseLocationActivity.java
@@ -13,6 +13,7 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import com.google.android.maps.GeoPoint;
@@ -41,6 +42,10 @@ public class ChooseLocationActivity extends MapActivity {
 		Criteria c = new Criteria();
 		c.setAccuracy(Criteria.ACCURACY_COARSE);
 		String provider = mLocationManager.getBestProvider(c, true);
+		
+		if (!mLocationManager.isProviderEnabled(provider)) {
+		    provider = LocationManager.NETWORK_PROVIDER;
+		}
 		mLocationManager.requestLocationUpdates(provider, 0, 0, new MapListener());
 	}
 
@@ -50,9 +55,15 @@ public class ChooseLocationActivity extends MapActivity {
 	}
 	
 	private class MapListener implements LocationListener {
+        private static final int TWO_MINUTES = 1000 * 60 * 2;
+	    private Location mCurrentBestLocation;
+	    
 		@Override
 		public void onLocationChanged(Location location) {
-			mLocationManager.removeUpdates(this);
+		    Log.i("MapListener", "Received a new location");
+		    if (isBetterLocation(location, mCurrentBestLocation)) {
+		        mLocationManager.removeUpdates(this);
+		    }
 			
 			GeoPoint p = new GeoPoint(
 				(int)(location.getLatitude()  * 1e6),
@@ -74,6 +85,63 @@ public class ChooseLocationActivity extends MapActivity {
 		@Override
 		public void onStatusChanged(String provider, int status, Bundle extras) {
 			// TODO Auto-generated method stub
+		}
+
+		/**
+		 * Determines whether one Location reading is better than the current Location fix
+		 * @param location  The new Location that you want to evaluate
+		 * @param currentBestLocation  The current Location fix, to which you want to compare the new one
+		 */
+		protected boolean isBetterLocation(Location location, Location currentBestLocation) {
+		    if (currentBestLocation == null) {
+		        // A new location is always better than no location
+		        return true;
+		    }
+
+		    // Check whether the new location fix is newer or older
+		    long timeDelta = location.getTime() - currentBestLocation.getTime();
+		    boolean isSignificantlyNewer = timeDelta > TWO_MINUTES;
+		    boolean isSignificantlyOlder = timeDelta < -TWO_MINUTES;
+		    boolean isNewer = timeDelta > 0;
+
+		    // If it's been more than two minutes since the current location, use the new location
+		    // because the user has likely moved
+		    if (isSignificantlyNewer) {
+		        return true;
+		    // If the new location is more than two minutes older, it must be worse
+		    } else if (isSignificantlyOlder) {
+		        return false;
+		    }
+
+		    // Check whether the new location fix is more or less accurate
+		    int accuracyDelta = (int) (location.getAccuracy() - currentBestLocation.getAccuracy());
+		    boolean isLessAccurate = accuracyDelta > 0;
+		    boolean isMoreAccurate = accuracyDelta < 0;
+		    boolean isSignificantlyLessAccurate = accuracyDelta > 200;
+
+		    // Check if the old and new location are from the same provider
+		    boolean isFromSameProvider = isSameProvider(location.getProvider(),
+		            currentBestLocation.getProvider());
+
+		    // Determine location quality using a combination of timeliness and accuracy
+		    if (isMoreAccurate) {
+		        return true;
+		    } else if (isNewer && !isLessAccurate) {
+		        return true;
+		    } else if (isNewer && !isSignificantlyLessAccurate && isFromSameProvider) {
+		        return true;
+		    }
+		    return false;
+		}
+
+		/**
+		 * Checks whether two providers are the same
+		 */
+		private boolean isSameProvider(String provider1, String provider2) {
+		    if (provider1 == null) {
+		      return provider2 == null;
+		    }
+		    return provider1.equals(provider2);
 		}
 	}
 	
