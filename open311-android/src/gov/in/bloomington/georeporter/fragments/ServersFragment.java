4b5aa879e853f35be593225f09b0465f41b23ab2
inghamn <inghamn@bloomington.in.gov>
2013-01-14 15:02:11 -0500

    Created an Add Server dialog on the settings screen for servers. Fixes #9

--- open311-android/src/gov/in/bloomington/georeporter/fragments/ServersFragment.java
+++ open311-android/src/gov/in/bloomington/georeporter/fragments/ServersFragment.java
@@ -5,9 +5,13 @@
  */
 package gov.in.bloomington.georeporter.fragments;
 
+import java.util.Arrays;
+import java.util.List;
+
 import gov.in.bloomington.georeporter.R;
 import gov.in.bloomington.georeporter.activities.MainActivity;
 import gov.in.bloomington.georeporter.adapters.ServersAdapter;
+import gov.in.bloomington.georeporter.models.Open311;
 import gov.in.bloomington.georeporter.models.Preferences;
 import gov.in.bloomington.georeporter.util.Util;
 
@@ -15,43 +19,189 @@ import gov.in.bloomington.georeporter.util.json.JSONArray;
 import gov.in.bloomington.georeporter.util.json.JSONException;
 import gov.in.bloomington.georeporter.util.json.JSONObject;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.LayoutInflater;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.View.OnCreateContextMenuListener;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.ToggleButton;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 
-import com.actionbarsherlock.app.SherlockListFragment;
+import com.actionbarsherlock.app.SherlockFragment;
 
-public class ServersFragment extends SherlockListFragment {
-	JSONArray mServers = null;
+public class ServersFragment extends SherlockFragment implements OnItemClickListener, OnCreateContextMenuListener {
+    private int       mNumAvailableServers = 0;
+    private JSONArray mCustomServers       = null;
+    private ListView  mListView;
+    
+    private static final List<String> FORMAT_CHOICES = Arrays.asList(Open311.JSON, Open311.XML);
+    
+    /**
+     * Available and Custom servers combined into one array
+     */
+	private JSONArray mServers = null;
 	
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		try {
-			mServers = new JSONArray(Util.file_get_contents(getActivity(), R.raw.available_servers));
-		} catch (JSONException e) {
-			Util.displayCrashDialog(getActivity(), "Could not load endpoints from json");
-		}
-		setListAdapter(new ServersAdapter(mServers, getActivity()));
+	public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+	    View v = inflater.inflate(R.layout.fragment_servers, container, false);
+        final View dialogLayout = inflater.inflate(R.layout.dialog_add_server, null);
+	    
+	    mListView = (ListView) v.findViewById(R.id.serversListView);
+	    mListView.setOnItemClickListener(this);
+	    mListView.setOnCreateContextMenuListener(this);
+	    v.findViewById(R.id.addServerButton).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Spinner format = (Spinner) dialogLayout.findViewById(R.id.format);
+                ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, FORMAT_CHOICES);
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+                format.setAdapter(adapter);
+                
+                AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+                builder.setView(dialogLayout)
+                       .setTitle(R.string.button_add_server)
+                       .setPositiveButton(R.string.save, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                saveNewServer(dialogLayout); // Update the Preferences data
+                                refresh();                   // Redraw the screen from Preferences
+                                dialog.dismiss();            // Close the dialog
+                            }
+                       })
+                       .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                       });
+                builder.show();
+            }
+        });
+	    refresh();
+	    return v;
+	}
+	
+	/**
+	 * Adds a new server JSONObject to Preferences
+	 * 
+	 * This only updates the Preferences data.  You still need make sure
+	 * that the screen actually reflects this change.
+	 * 
+	 * @param dialog
+	 */
+	private void saveNewServer(View dialog) {
+        final EditText     name           = (EditText)     dialog.findViewById(R.id.name);
+        final EditText     url            = (EditText)     dialog.findViewById(R.id.url);
+        final EditText     jurisdiction   = (EditText)     dialog.findViewById(R.id.jurisdiction);
+        final EditText     api_key        = (EditText)     dialog.findViewById(R.id.api_key);
+        final ToggleButton supports_media = (ToggleButton) dialog.findViewById(R.id.supports_media);
+        final Spinner      format         = (Spinner)      dialog.findViewById(R.id.format);
+        
+        JSONObject server = new JSONObject();
+        try {
+            server.put(Open311.NAME,           name        .getText().toString());
+            server.put(Open311.URL,            url         .getText().toString());
+            server.put(Open311.JURISDICTION,   jurisdiction.getText().toString());
+            server.put(Open311.API_KEY,        api_key     .getText().toString());
+            server.put(Open311.SUPPORTS_MEDIA, supports_media.isChecked());
+            server.put(Open311.FORMAT,         format.getSelectedItem());
+            
+            mCustomServers.put(server);
+            Preferences.setCustomServers(mCustomServers, getActivity());
+        }
+        catch (JSONException e) {
+            e.printStackTrace();
+        }
+	}
+	
+	/**
+	 * Reloads server data and updates the ListView
+	 * 
+	 * Reads servers from both available_servers and Preferences.custom_servers.
+	 * The list will have available_servers first, the custom_servers.
+	 * Remember that the custom_servers are editable, while the available_servers are not.
+	 */
+	private void refresh() {
+        try {
+            mServers = new JSONArray(Util.file_get_contents(getActivity(), R.raw.available_servers));
+            mNumAvailableServers = mServers.length();
+            
+            mCustomServers = Preferences.getCustomServers(getActivity());
+            int len = mCustomServers.length();
+            for (int i=0; i<len; i++) {
+                mServers.put(mCustomServers.getJSONObject(i));
+            }
+        } catch (JSONException e) {
+            Util.displayCrashDialog(getActivity(), "Could not load endpoints from json");
+        }
+        mListView.setAdapter(new ServersAdapter(mServers, getActivity()));
+	}
+	
+	/**
+	 * Sets the current server
+	 * 
+	 * When the user touches one of the servers, set that
+	 * one to be the current_server and send them to MainActivity
+	 */
+	@Override
+    public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+        JSONObject current_server = null;
+        try {
+            current_server = mServers.getJSONObject(position);
+        } catch (JSONException e) {
+            // We'll just pass null to Preferences, which will wipe current_server
+            // Once they get sent to Home, home will realize there isn't 
+            // a current_server and send them back here
+        }
+        Preferences.setCurrentServer(current_server, getActivity());
+        
+        Intent i = new Intent(getActivity(), MainActivity.class);
+        i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+        startActivity(i);
+        
+    }
+	
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v, ContextMenuInfo menuInfo) {
+	    super.onCreateContextMenu(menu, v, menuInfo);
+	    
+	    // Only show the delete menu for the custom servers.
+	    // Servers from available_servers cannot be deleted.
+	    AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) menuInfo;
+	    if (info.position >= mNumAvailableServers) {
+	        MenuInflater inflater = getActivity().getMenuInflater();
+	        inflater.inflate(R.menu.context_listitem, menu);
+	    }
 	}
 	
 	@Override
-	public void onListItemClick(ListView l, View v, int position, long id) {
-		super.onListItemClick(l, v, position, id);
-		
-		JSONObject current_server = null;
-		try {
-			current_server = mServers.getJSONObject(position);
-		} catch (JSONException e) {
-			// We'll just pass null to Preferences, which will wipe current_server
-			// Once they get sent to Home, home will realize there isn't 
-			// a current_server and send them back here
-		}
-		Preferences.setCurrentServer(current_server, getActivity());
-		
-		Intent i = new Intent(getActivity(), MainActivity.class);
-		i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
-		startActivity(i);
+	public boolean onContextItemSelected(MenuItem item) {
+        AdapterContextMenuInfo info = (AdapterContextMenuInfo) item.getMenuInfo();
+        
+        switch (item.getItemId()) {
+            case R.id.menu_delete:
+                int position = info.position - mNumAvailableServers;
+                mCustomServers.remove(position);
+                Preferences.setCustomServers(mCustomServers, getActivity());
+                refresh();
+                return true;
+                
+            default:
+                return super.onContextItemSelected(item);
+        }
 	}
 }
--- open311-android/src/gov/in/bloomington/georeporter/models/Preferences.java
+++ open311-android/src/gov/in/bloomington/georeporter/models/Preferences.java
@@ -21,6 +21,7 @@ import android.content.SharedPreferences;
 public class Preferences {
 	private static final String SETTINGS       = "settings";
 	private static final String PERSONAL_INFO  = "personal_info";
+	private static final String CUSTOM_SERVERS = "custom_servers";
 	
 	private static final String APP_STATE      = "app_state";
 	private static final String CURRENT_SERVER = "current_server";
@@ -48,8 +49,7 @@ public class Preferences {
 	 * ready for the user to start filling out the fields.
 	 * 
 	 * @param c
-	 * @return
-	 * JSONObject
+	 * @return JSONObject
 	 */
 	public static JSONObject getPersonalInfo(Context c) {
 		Preferences.loadSettings(c);
@@ -65,7 +65,6 @@ public class Preferences {
      * 
      * @param personal_info
      * @param c
-     * void
      */
 	public static void setPersonalInfo(JSONObject personal_info, Context c) {
         Preferences.loadSettings(c);
@@ -74,34 +73,98 @@ public class Preferences {
         editor.putString(PERSONAL_INFO, personal_info.toString());
         editor.commit();
     }
+	
+	/**
+	 * Returns any custom server definitions stored in settings
+	 * 
+	 * Users can add additional servers to the settings.
+	 * These additional server definitions will be stored as JSON
+	 * strings in settings.
+	 * 
+	 * @param c
+	 * @return JSONArray
+	 */
+	public static JSONArray getCustomServers(Context c) {
+	    Preferences.loadSettings(c);
+	    try {
+            return new JSONArray(mSettings.getString(CUSTOM_SERVERS, "[]"));
+        }
+        catch (JSONException e) {
+            return null;
+        }
+	}
+	
+	/**
+	 * Writes custom servers back to disk
+	 * 
+	 * @param custom_servers
+	 * @param c
+	 */
+	public static void setCustomServers(JSONArray custom_servers, Context c) {
+	    Preferences.loadSettings(c);
+	    
+	    SharedPreferences.Editor editor = mSettings.edit();
+	    editor.putString(CUSTOM_SERVERS, custom_servers.toString());
+	    editor.commit();
+	}
     
 	/**
 	 * Returns the current_server stored in app_state
 	 * 
 	 * This may return null, meaning there is no current_server chosen.
 	 * 
+	 * Server definitions will change over time, and we always want to use
+	 * the latest defintion of each server. Check for the server by name
+	 * and fully reload the JSON each time.
+	 * 
 	 * @param context
 	 * @return JSONObject
 	 */
 	public static JSONObject getCurrentServer(Context context) {
 		Preferences.loadState(context);
-		try {
-		    String serverName = mState.getString(CURRENT_SERVER, "");
-		    if (serverName != null) {
-		        JSONArray available_servers = new JSONArray(Util.file_get_contents(context, R.raw.available_servers));
-		        int len = available_servers.length();
-		        for (int i=0; i<len; i++) {
-		            JSONObject s = available_servers.getJSONObject(i);
-		            if (s.getString(Open311.NAME).equals(serverName)) {
-		                return s;
-		            }
-		        }
-		    }
-		}
-		catch (JSONException e) {
-			e.printStackTrace();
-		}
+	    String serverName = mState.getString(CURRENT_SERVER, "");
+	    if (serverName != null) {
+            JSONObject s = null;
+            
+            try {
+                JSONArray available_servers = new JSONArray(Util.file_get_contents(context, R.raw.available_servers));
+                s = findServerByName(available_servers, serverName);
+                if (s != null) return s;
+            }
+            catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+	        
+	        s = findServerByName(getCustomServers(context), serverName);
+	        if (s != null) return s;
+	    }
 		return null;
+	}
+	
+	/**
+	 * Loops through a JSONArray and returns the match, based on the name
+	 * 
+	 * @param servers
+	 * @param name
+	 * @return JSONObject
+	 */
+	private static JSONObject findServerByName(JSONArray servers, String name) {
+	    int len = servers.length();
+        JSONObject s;
+	    for (int i=0; i<len; i++) {
+            try {
+                s = servers.getJSONObject(i);
+                if (s.getString(Open311.NAME).equals(name)) {
+                    return s;
+                }
+            }
+            catch (JSONException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+	    }
+	    return null;
 	}
 	
 	/**
