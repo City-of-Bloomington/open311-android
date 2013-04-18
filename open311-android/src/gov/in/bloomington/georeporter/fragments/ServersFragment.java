25de43f04fcb28c560d5b049fd6494bc7df169c7
inghamn <inghamn@bloomington.in.gov>
2013-04-18 10:42:52 -0400

    Code formatting cleanup

--- open311-android/src/gov/in/bloomington/georeporter/fragments/ServersFragment.java
+++ open311-android/src/gov/in/bloomington/georeporter/fragments/ServersFragment.java
@@ -66,13 +66,10 @@ public class ServersFragment extends SherlockFragment implements OnItemClickList
 	    v.findViewById(R.id.addServerButton).setOnClickListener(new OnClickListener() {
             @Override
             public void onClick(View v) {
-            	
-						// Check if the view on which AlertDialog is called has
-						// a parent and remove that parent View
-						if (dialogLayout.getParent() != null) {
-							((ViewGroup) dialogLayout.getParent())
-									.removeView(dialogLayout);
-						}
+                // Remove the previous instance of the dialog from the parent view
+				if (dialogLayout.getParent() != null) {
+					((ViewGroup) dialogLayout.getParent()).removeView(dialogLayout);
+				}
 				
                 final Spinner format = (Spinner) dialogLayout.findViewById(R.id.format);
                 ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_spinner_item, FORMAT_CHOICES);
